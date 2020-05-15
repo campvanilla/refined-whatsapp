129934a584b45ec10d3ca294d0308f5e12e922bc
Abinav Seelan <abinav.n.seelan@gmail.com>
2020-05-15 22:10:48 +0530

    feature: Overnight

--- src/content-scripts/index.ts
+++ src/content-scripts/index.ts
@@ -6,18 +6,18 @@ console.clear();
 
 document.addEventListener('keydown', handleHotKeys);
 
-const poller = setInterval(() => {
-  const controlList = document.getElementById('side') as HTMLDivElement;
-  if (controlList) {
-    clearInterval(poller);
-    const themeBtn = document.createElement('BUTTON');
-    themeBtn.innerText = 'Change Theme';
-    themeBtn.addEventListener('click', () => {
-      setTheme(Theme.OvernightSlumber);
-    });
+// const poller = setInterval(() => {
+//   const controlList = document.getElementById('side') as HTMLDivElement;
+//   if (controlList) {
+//     clearInterval(poller);
+//     const themeBtn = document.createElement('BUTTON');
+//     themeBtn.innerText = 'Change Theme';
+//     themeBtn.addEventListener('click', () => {
+//       setTheme(Theme.OvernightSlumber);
+//     });
 
-    controlList.children[0].appendChild(themeBtn);
-  }
-}, 1000);
+//     controlList.children[0].appendChild(themeBtn);
+//   }
+// }, 1000);
 
 setTheme(Theme.OvernightSlumber);
--- src/theme/overnight.ts
+++ src/theme/overnight.ts
@@ -1,95 +1,57 @@
-/**
- * Colors:
- * rgb(36, 36, 36)
- * #151515
- * #1C2022
- * #434343
- * #171717
- */
-
 export const OvernightSlumber = `
   :root {
-    --gray-60: #101d25;
-    --gray-100:#232d36;
-    --gray-400:#232d36;
-    --gray-600: #cbd4d9;
-    --gray-700: #cbd4d9;
-    --primary: #cbd4d9;
-    --border-stronger: rgba(0, 0, 0, 0);
-    --primary-rgb: 203, 212, 217;
-    --border-default: #101d25;
-    --border-list: rgba(0, 0, 0, 0);
-    --border-list-rgb: 16, 29, 37;
+    --app-background-stripe: #151515;
+    --panel-background-colored: #747474;
+    --primary: #eee;
+    --border-default: rgb(36, 36, 36);
+    --butterbar-default-background: #747474;
+    --panel-background-rgb: 36, 36, 36;
+    --drawer-background: rgb(36, 36, 36);
+    --drawer-section-background: #2c2c2c;
+    --incoming-background: rgb(36, 36, 36);
+    --incoming-background-rgb: 67, 67, 67;
+    --incoming-background-deeper: rgb(36, 36, 36);
+    --outgoing-background: rgb(36, 36, 36);
+    --outgoing-background-rgb: 67, 67, 67;
+    --outgoing-background-deeper: rgb(36, 36, 36);
+    --outgoing-background-deeper-rgb: 67, 67, 67;
+    --dropdown-background: rgb(36, 36, 36);
+    --panel-background-deep: #434343;
+    --drawer-gallery-background: #242424;
+    --compose-background: #434343;
+    --message-selection-highlight: rgba(0,0,0, 0.2);
+    --modal-backdrop: rgba(0,0,0, 0.6);
+    --modal-background: rgb(36, 36, 36);
+    --button-primary-background: #434343;
+    --button-primary-background-hover: #434343;
+    --button-primary: #cbd4d9;
+    --button-secondary: #434343;
+    --button-secondary-hover: #cbd4d9;
+    --button-secondary-background-hover: #434343;
+    --rich-text-panel-background: rgb(36, 36, 36);
+    --panel-background-hover: #232d36;
+    --panel-background-hover-rgb: 35, 45, 54;
+    --background-default-active: #151515;
+    --drawer-background-rgb: 16, 29, 37;
+    --drawer-section-background-rgb: 16, 29, 37;
     --conversation-panel-background: #151515;
     --intro-background: #151515;
     --background-default: rgb(36, 36, 36);
-    --border-panel: #101d25;
-    --background-default-active: #171717;
-    --background-default-hover: #434343;
-    --border-chatlist: #101d25;
-    --butterbar-background-default: #151515;
-    --rich-text-panel-background: rgb(36, 36, 36);
     --panel-background-lighter: rgb(36, 36, 36);
     --panel-background: rgb(36, 36, 36);
-    --panel-background-rgb: 35, 45, 54;
-    --panel-background-hover: #232d36;
-    --panel-background-hover-rgb: 35, 45, 54;
-    --drawer-background: #101d25;
-    --drawer-background-rgb: 16, 29, 37;
-    --drawer-section-background: #101d25;
-    --drawer-section-background-rgb: 16, 29, 37;
-    --primary-strong: #d1d8dd;
+    --background-default-hover: #434343;
+    --primary-strong: #eee;
     --primary-strong-rgb: 209, 216, 221;
-    --primary-stronger: #cbd4d9;
-    --secondary: #8c959a;
-    --secondary-stronger:: #8c959a;
-    --secondary-lighter: #8c959a;
-    --icon-lighter-rgb: #FFF;
-    --icon-lighter: #8c959a;
-    --icon-strong: #d1d8dd;
-
-    --system-message-background: #232d36;
-    --notification-biz-background: #232d36;
-    --notification-text: #d1d8dd;
-    --system-message-text: #d1d8dd;
-    --incoming-background: #232d36;
-    --incoming-background-deeper: #232d36;
-    --outgoing-background: #232d36;
-    --outgoing-background-rgb: (16, 29, 37);
-    --outgoing-background-deeper: #232d36;
-    --outgoing-background-deeper-rgb: (16, 29, 37);
-    --icon-strong-rgb: #d1d8dd;
-    --unread-background: rgb(0,0,0,0);
-    --unread-bar-background: rgba(35,45,54,0.25);
-    --white: #232d36;
-    --white-rgb: (35,45,54);
-    --avatar-placeholder-primary: #232d36;
-    --avatar-placeholder-primary-rgb: (35,45,54);
-    --media-viewer-button-icon: #232d36;
-    --media-viewer-button-icon-rgb: (35,45,54);
-    --button-primary: #232d36;
-    --button-secondary-background: #232d36;
-    --modal-background: #232d36;
-    --modal-backdrop-solid: #101d25;
-    --status-primary: #232d36;
-    --dropdown-background:#232d36;
-    --dropdown-background-hover: #414a51;
-    --dropdown-background-hover-rgb: 65, 74, 81;
-    --notification-e2e-text: #d1d8dd;
-    --notification-e2e-background: #232d36;
-    --modal-backdrop: rgba(16, 29, 37,0.85);
-    --panel-background-deep: #101d25;
-    --panel-background-deeper:#232d36;
-    --audio-track-incoming:#414a51;
-    --audio-track-outgoing: #414a51;
-    --avatar-background:#232d36;
-    --button-background-disabled:#232d36;
-    --chat-marker: #232d36;
-
-    --media-viewer-background: #101d25;
-    --media-viewer-background-rgb: (16, 29, 37);
-
-    --compose-background: #232d36;
+    --primary-stronger: #fff;
+    --secondary: #ddd;
+    --secondary-stronger:: #ddd;
+    --secondary-lighter: #ddd;
+    --icon-lighter: #ddd;
+    --icon-lighter-rgb: #ddd;
+    --icon-strong-rgb: #fff;
+    --border-list: rgba(0, 0, 0, 0);
+    --system-message-background: #434343;
+    --system-message-text: #fff;
   }
   body {
       color: #d1d8dd;
@@ -102,41 +64,7 @@ export const OvernightSlumber = `
   .app-wrapper-web {
       background-color: #1C2022;
   }
-  ._347-w::after {
-      background-color: #1C2022 !important;
-  }
-  ._2y17h {
-      border-right: none;
-  }
-  .m6ZEb {
-      display: none !important;
-  }
-  .YUoyu {
-      opacity: 0.03 !important;
-      background-color: white;
-  }
 
-  .MVjBr {
-      background-color: #232d36 !important;
-  }
-  .tail-container {
-      display: none !important;
-  }
-  ._3Ye_R {
-      color: #d1d8dd;
-  }
-  ._1i1U7.jZ4tp {
-      background: none !important;
-  }
-  .JWJN5 {
-      background-color: rgba(16, 29, 37, 0.8) !important;
-  }
-  .message-in ._3JaNE ._2R8MP, html[dir] .message-in ._3JaNE ._2Ll5c {
-      background-color: #232d36 !important;
-  }
-  ._30525 {
-      color: #d1d8dd;
-  }
   #startup, #initial_startup {
     background-color: rgb(36, 36, 36) !important;
   }
